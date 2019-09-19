e019e6871ed8c3c0504e19a5311ce16c185d5d21
Kerman Kohli <kermankohli@gmail.com>
2019-09-19 17:05:22 +1000

    Minor refactor

---
+++ packages/serverless/services/BalanceService.ts
@@ -0,0 +1,37 @@
+import { Provider } from 'ethers/providers';
+import { BigNumber } from 'ethers/utils';
+import { asyncForEach } from '../utils/array';
+import { ethers } from 'ethers';
+import { erc20 } from '../abis/ts/erc20'
+import { getKeyByValue } from '../utils/dictionary';
+
+const divider = new BigNumber(10).pow(18);
+
+class BalanceService {
+
+  provider: Provider;
+  tokens: {[ticker: string]: string};
+
+  constructor(provider: Provider, tokens: {[ticker: string]: string}) {
+    this.provider = provider;
+    this.tokens = tokens;
+  }
+
+  async retrieveAllBalances(address: string): Promise<{[ticker: string]: number}> {
+    let balances: {[ticker: string]: number} = {};
+    balances['ETH'] = (await this.provider.getBalance(address)).div(divider).toNumber();
+    
+    await asyncForEach(Object.values(this.tokens), async (tokenAddress: string) => {
+      const contract = new ethers.Contract(tokenAddress, erc20, this.provider);
+      const balance = await contract.functions.balanceOf(address);
+      const decimals = await contract.functions.decimals();
+      const tokenTicker = getKeyByValue(this.tokens, tokenAddress);
+      balances[tokenTicker] = balance.div(new BigNumber(10).pow(decimals)).toNumber();
+    });
+    
+    return balances;
+  }
+
+}
+
+export default BalanceService;
--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -1,27 +1,19 @@
-import { moloch } from './../abis/ts/moloch';
-import { daoAddresses, tokenAddresses } from './../constants/addresses';
-import {ethers} from 'ethers';
-import { BigNumber } from 'ethers/utils';
-import { asyncForEach } from '../utils/array';
-
-import { erc20 } from '../abis/ts/erc20'
+import { daos, tokens } from './../constants/addresses';
+import { ethers } from 'ethers';
+import { getKeyByValue } from '../utils/dictionary';
 
 import PriceService from '../services/PriceService';
+import BalanceService from '../services/BalanceService';
 
 require('dotenv').config({ path: './.env' });
 
 const provider = new ethers.providers.InfuraProvider(1, process.env.INFURA_KEY);
-
-let divider = new BigNumber(10).pow(18);
 
 let results = {};
 
 async function fetch() {
   console.log(`Current INFURA_KEY is ${process.env.INFURA_KEY}`);
   
-  const daos = daoAddresses;
-  const tokens = tokenAddresses;
-
   Object.values(daos).forEach(dao => {
     results[dao] = {
       name: getKeyByValue(daos, dao),
@@ -30,60 +22,65 @@ async function fetch() {
     };
   });
   
-  let finalTickers: string[] = Object.keys(tokenAddresses);
+  let finalTickers: string[] = Object.keys(tokens);
   finalTickers.push('ETH');
-  const prices = await new PriceService().getPrices(finalTickers);
+
+  const priceService = new PriceService();
+  const prices = await priceService.getPrices(finalTickers);
   console.log(prices);
 
-  await ethBalances(prices);
-  await tokenBalances(prices);
+  const balanceService = new BalanceService(provider, tokens);
+  const aragonBalance = await balanceService.retrieveAllBalances(daos.aragon);
+  console.log(aragonBalance);
+  // await ethBalances(prices);
+  // await tokenBalances(prices);
 
-  Object.keys(results).forEach(key => {
-    const item = results[key];
-    let balances = Object.values(item['balances']);
-    results[key]['total'] = balances.reduce((previous: string, current: string) => parseFloat(previous) + parseFloat(current));
-  });
+  // Object.keys(results).forEach(key => {
+  //   const item = results[key];
+  //   let balances = Object.values(item['balances']);
+  //   results[key]['total'] = balances.reduce((previous: string, current: string) => parseFloat(previous) + parseFloat(current));
+  // });
 
   console.log(results);
   
 }
 
-async function ethBalances(prices: any) {
-  const daos = Object.values(daoAddresses);
-  let list: any = {};
-  await asyncForEach(daos, async (address: string) => {
-    const balance = await provider.getBalance(address);
-    const usdValue = balance.div(divider).toNumber() * prices['ETH'];
-    // console.log(`DAO: ${address} = ETH: $${usdValue.toString()}`);
-    results[address]['balances']['ETH'] = usdValue.toString();
-  });
-}
+// async function ethBalances(prices: any) {
+//   const daos = Object.values(daoAddresses);
+//   let list: any = {};
+//   await asyncForEach(daos, async (address: string) => {
+//     const balance = await provider.getBalance(address);
+//     const usdValue = balance.div(divider).toNumber() * prices['ETH'];
+//     // console.log(`DAO: ${address} = ETH: $${usdValue.toString()}`);
+//     results[address]['balances']['ETH'] = usdValue.toString();
+//   });
+// }
 
-async function tokenBalances(prices: any) {
-  const daos = Object.values(daoAddresses);
-  const tokens = Object.values(tokenAddresses);
+// async function tokenBalances(prices: any) {
+//   const daos = Object.values(daoAddresses);
+//   const tokens = Object.values(tokenAddresses);
 
-  await asyncForEach(daos, async (daoAddress: string) => {
-    await asyncForEach(tokens, async (tokenAddress: string) => {
-      const contract = new ethers.Contract(tokenAddress, erc20, provider);
-      const balance = await contract.functions.balanceOf(daoAddress);
-      let tokenTicker = getKeyByValue(tokenAddresses, tokenAddress);
-      let fetchTicker = tokenTicker;
-      if (tokenTicker == 'CDAI') {
-        fetchTicker = 'DAI';
-      }
-      if (tokenTicker == 'WETH') {
-        fetchTicker = 'ETH';
-      }
-      const usdValue = balance.div(divider).toNumber() * prices[fetchTicker];
-      results[daoAddress]['balances'][tokenTicker] = usdValue.toString();
-      // console.log(`DAO: ${daoAddress} = ${tokenTicker}: ${usdValue.toString()}`);
-    });
-  });
-}
+//   await asyncForEach(daos, async (daoAddress: string) => {
+//     await asyncForEach(tokens, async (tokenAddress: string) => {
+//       const contract = new ethers.Contract(tokenAddress, erc20, provider);
+//       const balance = await contract.functions.balanceOf(daoAddress);
+//       let tokenTicker = getKeyByValue(tokenAddresses, tokenAddress);
+//       let fetchTicker = tokenTicker;
+//       if (tokenTicker == 'CDAI') {
+//         fetchTicker = 'DAI';
+//       }
+//       if (tokenTicker == 'WETH') {
+//         fetchTicker = 'ETH';
+//       }
+//       const usdValue = balance.div(divider).toNumber() * prices[fetchTicker];
+//       results[daoAddress]['balances'][tokenTicker] = usdValue.toString();
+//       // console.log(`DAO: ${daoAddress} = ${tokenTicker}: ${usdValue.toString()}`);
+//     });
+//   });
+// }
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
-}
+// function getKeyByValue(object, value) {
+//   return Object.keys(object).find(key => object[key] === value);
+// }
 
 fetch();
